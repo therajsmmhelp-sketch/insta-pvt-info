9a1b6430ebf6ed6178092f00b458958ba07423eb
Raj <therajsmmhelp@gmail.com>
2026-07-10 10:43:14 +0530

    Updated project

--- app/page.tsx
+++ app/page.tsx
@@ -93,7 +93,7 @@ export default function HomePage() {
             
               Account
            {" "}
-            Viewer by Raj
+            Viewer
           </motion.h2>
           <motion.p
             initial={{ opacity: 0, y: 14 }}
